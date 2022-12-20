3b1ac4fb3a6c73f62019b910d32d5f340c34a5ac
Pedro Lucas <falecompedrodev@gmail.com>
2022-12-20 04:03:32 -0300

    Envio de arquivos

--- crud_JavaMysql/src/br/com/check/aplicacao/Main.java
+++ crud_JavaMysql/src/br/com/check/aplicacao/Main.java
@@ -1,9 +1,30 @@
 package br.com.check.aplicacao;
 
+import java.util.Date;
+
+import br.com.check.dao.ContatoDAO;
+import br.com.check.model.Contato;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ContatoDAO contatoDao = new ContatoDAO();
+		
+		Contato contato = new Contato();
+		contato.setNome("Pedro");
+		contato.setId(3);
+		contato.setDataCadastro(new Date());
+		contato.setIdade(19);
+		
+		Contato contato1 = new Contato();
+		contato.setNome("Teste");
+		contato.setId(5);
+		contato.setDataCadastro(new Date());
+		contato.setIdade(20);
+		
+		contatoDao.save(contato);
 
 	}
 
